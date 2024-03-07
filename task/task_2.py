61273c02bddeb9a3c4870699ad3327b77c142f6c
Lab228 <ray12099@gmail.com>
2024-03-07 15:11:07 +0800

    20240307

--- task/task_2.py
+++ task/task_2.py
@@ -11,19 +11,29 @@ from func_2 import *
 task2 = os.path.join(r"C:\Program Files\114\txtfile\task_2\path.txt")
 psspy.psseinit(0)
 psspy.case(r'C:\Program Files\114\savfile\task_2.sav')            # opening sav file
-ierr, busnum = psspy.abusint(-1, 1, 'NUMBER')                     # only in-serve bus numbers
-ierr, iarray_from = psspy.abrnint(-1, 1, 3, 3, 1, 'FROMNUMBER')   # only in-serve branch
-ierr, iarray_to = psspy.abrnint(-1, 1, 3, 3, 1, 'TONUMBER')
-iarray_from = iarray_from[0]
-iarray_to = iarray_to[0]
-busnum = busnum[0]
+ierr, busnum        = psspy.abusint(-1, 1, 'NUMBER')                     # only in-serve bus numbers
+ierr, iarray_from   = psspy.abrnint(-1, 1, 3, 1, 1, 'FROMNUMBER')   # only in-serve branch
+ierr, iarray_to     = psspy.abrnint(-1, 1, 3, 1, 1, 'TONUMBER')
+ierr, iarray_from_2 = psspy.atrnint(-1, 1, 3, 1, 1, 'FROMNUMBER')
+ierr, iarray_to_2   = psspy.atrnint(-1, 1, 3, 1, 1, 'TONUMBER')
+ierr, iarray_from_3 = psspy.atr3int(-1, 1, 3, 1, 1, 'WIND1NUMBER')
+ierr, iarray_to_3   = psspy.atr3int(-1, 1, 3, 1, 1, 'WIND2NUMBER')
+ierr, iarray_last_3 = psspy.atr3int(-1, 1, 3, 1, 1, 'WIND3NUMBER')
+iarray_from   = iarray_from[0]
+iarray_to     = iarray_to[0]
+iarray_from_2 = iarray_from_2[0]
+iarray_to_2   = iarray_to_2[0]
+iarray_from_3 = iarray_from_3[0]
+iarray_to_3   = iarray_to_3[0]
+iarray_last_3 = iarray_last_3[0]
+busnum        = busnum[0]
 
 root = Tk()
 root.update()
-stt = askinteger("Input", "Enter Starting Bus no.", initialvalue=210)
+stt = askinteger("Input", "Enter Starting Bus no.", initialvalue=107)
 end = askinteger("Input", "Enter Ending Bus no.", initialvalue=1090)
 lev = askinteger(
-    "Input", "Enter voltage level in KV's you want to omit", initialvalue=161)
+    "Input", "Enter voltage level in KV's you want to omit", initialvalue=1)
 sys.stdout = open(task2, 'w')
 
 #output path
@@ -39,6 +49,32 @@ for i in range(0,len(busnum)):
         if (iarray_to[j] == busnum[i]) and (iarray_from[j] not in path[busnum[i]]):
             ierr, rval = psspy.busdat(iarray_from[j], 'BASE')
             if rval > lev : path[busnum[i]] += [iarray_from[j]]
+    for j in range(0,len(iarray_from_2)):        
+        if (iarray_from_2[j] == busnum[i]) and (iarray_to_2[j] not in path[busnum[i]]):
+            ierr, rval = psspy.busdat(iarray_to_2[j], 'BASE')
+            if rval > lev : path[busnum[i]] += [iarray_to_2[j]]
+        if (iarray_to_2[j] == busnum[i]) and (iarray_from_2[j] not in path[busnum[i]]):
+            ierr, rval = psspy.busdat(iarray_from_2[j], 'BASE')
+            if rval > lev : path[busnum[i]] += [iarray_from_2[j]]
+    for j in range(0,len(iarray_from_3)):        
+        if (iarray_from_3[j] == busnum[i]) and (iarray_to_3[j] not in path[busnum[i]]):
+            ierr, rval = psspy.busdat(iarray_to_3[j], 'BASE')
+            if rval > lev : path[busnum[i]] += [iarray_to_3[j]]
+        if (iarray_from_3[j] == busnum[i]) and (iarray_last_3[j] not in path[busnum[i]]):
+            ierr, rval = psspy.busdat(iarray_last_3[j], 'BASE')
+            if rval > lev : path[busnum[i]] += [iarray_last_3[j]]
+        if (iarray_to_3[j] == busnum[i]) and (iarray_from_3[j] not in path[busnum[i]]):
+            ierr, rval = psspy.busdat(iarray_from_3[j], 'BASE')
+            if rval > lev : path[busnum[i]] += [iarray_from_3[j]]
+        if (iarray_to_3[j] == busnum[i]) and (iarray_last_3[j] not in path[busnum[i]]):
+            ierr, rval = psspy.busdat(iarray_last_3[j], 'BASE')
+            if rval > lev : path[busnum[i]] += [iarray_last_3[j]]
+        if (iarray_last_3[j] == busnum[i]) and (iarray_from_3[j] not in path[busnum[i]]):
+            ierr, rval = psspy.busdat(iarray_from_3[j], 'BASE')
+            if rval > lev : path[busnum[i]] += [iarray_from_3[j]]  
+        if (iarray_last_3[j] == busnum[i]) and (iarray_to_3[j] not in path[busnum[i]]):
+            ierr, rval = psspy.busdat(iarray_to_3[j], 'BASE')
+            if rval > lev : path[busnum[i]] += [iarray_to_3[j]]      
     if (path[busnum[i]] == []):
         del path[busnum[i]]
 
@@ -56,6 +92,8 @@ else :
     print('Following paths exist between start and end bus after omitting buses of level : '+ str(lev) + " KV\n")
     print('**********************************************************************\n')
     for w in range(0,len(ab)):
+        for l in range(0,len(ab[w])):
+            if ab[w][l]
         print("path " + str(w+1) + " : " + str(ab[w]))
 
 sys.stdout.close()
