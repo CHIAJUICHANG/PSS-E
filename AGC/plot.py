8e8a42a91fd21220134634ccb61fb3519ea86dba
Lab228 <ray12099@gmail.com>
2024-03-21 16:15:28 +0800

    20240321

--- AGC/plot.py
+++ AGC/plot.py
@@ -47,11 +47,11 @@ for i in range(0, NDM*4+2):
 
 # ----------------------run dyrnamic simulation-------------------------
 psspy.strt(0,outfile)
-psspy.run (0,  1, 10000, 10000, 0)
+psspy.run (0,  1, 1000, 1000, 0)
 psspy.dist_machine_trip(107, r"1")  # 423
 # psspy.dist_machine_trip(321, r"1")  # 270
 # psspy.dist_machine_trip(301, r"1")  # 150
-psspy.run (0, 180, 10000, 10000, 0)
+psspy.run (0, 80, 1000, 1000, 0)
 
 # ----------------------plot-------------------------
 chnfobj = dyntools.CHNF(outfile)
@@ -115,7 +115,7 @@ for i in range(5, NDM+5):
         plt.figure (4*(i-4)+j+1)
         if j == 0:
             plt.plot   (chandata['time'], freq, label='P(I)')
-            pmaxx      = [pmax(i-5) for k in range(0, len(chandata['time']))]
+            pmaxx      = [pmax[i-5] for k in range(0, len(chandata['time']))]
             plt.plot   (chandata['time'], pmaxx, label='Pmax')
             print('P('+str(i-4)+')'+str(freq[len(freq)-1]))
         if j == 1:    
